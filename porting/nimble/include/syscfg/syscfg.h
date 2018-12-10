38ec0618ba0fa5fffa4d85d372f95afc01ddc6e6
Sagar Bijwe <sagar@espressif.com>
2018-12-10 19:32:53 +0530

    esp-nimble: Conditionally include nimble config for ESP platform
    This change segregates nimble config for ESP platform from upstream
    config. This should ease process of maintaining esp-nimble repository.

--- porting/nimble/include/syscfg/syscfg.h
+++ porting/nimble/include/syscfg/syscfg.h
@@ -5,6 +5,10 @@
 #ifndef H_MYNEWT_SYSCFG_
 #define H_MYNEWT_SYSCFG_
 
+#ifdef ESP_PLATFORM
+#include "esp_nimble_cfg.h"
+#else
+
 /**
  * This macro exists to ensure code includes this header when needed.  If code
  * checks the existence of a setting directly via ifdef without including this
@@ -57,7 +61,7 @@
 #endif
 
 #ifndef MYNEWT_VAL_MSYS_1_BLOCK_COUNT
-#define MYNEWT_VAL_MSYS_1_BLOCK_COUNT (12)
+#define MYNEWT_VAL_MSYS_1_BLOCK_COUNT (20)
 #endif
 
 #ifndef MYNEWT_VAL_MSYS_1_BLOCK_SIZE
@@ -368,7 +372,7 @@
 #endif
 
 #ifndef MYNEWT_VAL_BLE_MAX_CONNECTIONS
-#define MYNEWT_VAL_BLE_MAX_CONNECTIONS (2)
+#define MYNEWT_VAL_BLE_MAX_CONNECTIONS (4)
 #endif
 
 #ifndef MYNEWT_VAL_BLE_MAX_PERIODIC_SYNCS
@@ -1405,7 +1409,7 @@
 #endif
 
 #ifndef MYNEWT_VAL_BLE_HCI_EVT_HI_BUF_COUNT
-#define MYNEWT_VAL_BLE_HCI_EVT_HI_BUF_COUNT (8)
+#define MYNEWT_VAL_BLE_HCI_EVT_HI_BUF_COUNT (30)
 #endif
 
 #ifndef MYNEWT_VAL_BLE_HCI_EVT_LO_BUF_COUNT
@@ -1480,5 +1484,5 @@
 #ifndef MYNEWT_VAL_TARGET_porting_default
 #define MYNEWT_VAL_TARGET_porting_default (1)
 #endif
-
+#endif
 #endif
